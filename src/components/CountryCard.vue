25d1a7b15e4601442520eb78f1a399f0d39b086e
Tomasz Kłusak <klusaktomasz@gmail.com>
2020-06-29 18:45:31 +0200

    Add fetching method for form and boilerplate for country card

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <article class="country">
+    <header class="country__header">
+      <h1 class="country__name">{{ name }}</h1>
+      <p class="country__full-name">{{ fullName }}</p>
+    </header>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'CountryCard',
+  props: {
+    name: { type: String, required: true },
+    fullName: { type: String, required: true },
+    code: { type: String, required: true },
+    capital: { type: String, required: true },
+    region: { type: String, required: true },
+    population: { type: String, required: true },
+    domain: { type: String, required: true },
+    languages: { type: Array, required: true },
+    currencies: { type: Array, required: true },
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/components/CountryForm.vue
+++ src/components/CountryForm.vue
@@ -7,20 +7,59 @@
     >
       Country name:
     </LabeledInput>
+
+    <output class="form__output">
+      <country-card
+        v-if="data"
+        :name="data.nativeName"
+        :fullName="data.altSpellings[2]"
+        :code="data.alpha3Code"
+        :capital="data.capital"
+        :region="data.region"
+        :population="data.population"
+        :domain="data.topLevelDomain[0]"
+        :languages="data.languages"
+        :currencies="data.currencies"
+      ></country-card>
+    </output>
   </form>
 </template>
 
 <script>
 import LabeledInput from '@/components/LabeledInput.vue';
+import CountryCard from '@/components/CountryCard.vue';
+
+import { RESTCOUNTRIES_GET_BY_NAME } from '@/utils/apiEndpoints';
 
 export default {
   name: 'CountryForm',
   components: {
     LabeledInput,
+    CountryCard,
   },
   data: () => ({
     countryName: '',
+    data: null,
+    isLoading: false,
+    error: false,
   }),
+  methods: {
+    async fetchCountryData(name) {
+      this.isLoading = true;
+      const res = await fetch(`${RESTCOUNTRIES_GET_BY_NAME}/${name}`);
+      this.isLoading = false;
+
+      if (!res.ok) {
+        this.error = true;
+        return null;
+      }
+
+      return res.json();
+    },
+  },
+  created() {
+    // this.fetchCountryData('');
+  },
 };
 </script>
 
